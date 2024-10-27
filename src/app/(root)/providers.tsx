30103950e4c7885bb82a9d273af4001a14be0c77
cocozzang <cocozz503@naver.com>
2024-10-27 11:09:13 +0900

    chore: react query setting

--- src/app/(root)/providers.tsx
+++ src/app/(root)/providers.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { initializeI18n } from "../../lib/utils";
+import { getQueryClient } from "./get-query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  initializeI18n();
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
