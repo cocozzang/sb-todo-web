28ceb9aede2143c1636e6502ca3e6265d8b60f6a
cocozzang <cocozz503@naver.com>
2024-11-11 09:25:32 +0900

    fix: 미들웨어 경로가드 조건에 user.info추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { SESSION_COOKIE_NAME } from "./const";
+import { SESSION_COOKIE_NAME, USER_INFO_COOKIE_NAME } from "./const";
 
 const protectedRoutes = ["/", "/user"];
 
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
-  const cookie = cookies().get(SESSION_COOKIE_NAME);
+  const sessionCookie = cookies().get(SESSION_COOKIE_NAME);
+  const userCookie = cookies().get(USER_INFO_COOKIE_NAME);
 
-  if (isProtectedRoute && !cookie) {
+  if (isProtectedRoute && (!sessionCookie || !userCookie)) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
